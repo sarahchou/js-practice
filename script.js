cd4fa564bdb756cef5f4cbf44c3a91fa553342ae
sarahchou <chou.s@husky.neu.edu>
2020-04-07 15:13:32 -0700

    random colors

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ function setButtonColor(button, red, green, blue) {
     button.setAttribute('style', 'background-color: rgb('+ red + ',' + green + ',' + blue + ');'
     );
 }
-setButtonColor(buttons[0], 0, 0, 255);
+
+function makeColorValue() {
+    return Math.round(Math.random()*255);
+}
+var red = makeColorValue();
+var green = makeColorValue();
+var blue = makeColorValue();
+setButtonColor(buttons[0], red, green, blue);
+setButtonColor(buttons[1], 40, 255, 25);
+setButtonColor(buttons[2], 180, 0, 240);
+setButtonColor(buttons[3], 210, 235, 100);
