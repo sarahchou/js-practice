6e0c643116dc4226e922f8f4a28407ab9e31c95f
sarahchou <chou.s@husky.neu.edu>
2020-04-07 17:36:26 -0700

    color button implementation

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var buttons = document.getElementsByClassName('colorButton');
-var heading;
-heading = document.getElementById('colorValue');
+//Functions
 function setButtonColor(button, red, green, blue) {
     button.setAttribute('style', 'background-color: rgb('+ red + ',' + green + ',' + blue + ');'
     );
@@ -10,9 +8,31 @@ function makeColorValue() {
     return Math.round(Math.random()*255);
 }
 
+//Variables
+var buttons = document.getElementsByClassName('colorButton');
+var heading = document.getElementById('colorValue');
+var answerMessage = document.getElementById('answer');
+var answerButton = Math.round(Math.random()*(buttons.length - 1));
+
+//Setting up each color value button
 for (var i = 0; i < buttons.length; i ++) {
     var red = makeColorValue();
     var green = makeColorValue();
     var blue = makeColorValue();
     setButtonColor(buttons[i], red, green, blue);
+
+    if (i == answerButton) {
+        heading.innerHTML = `(${red},${green},${blue})`;
+    }
+
+    buttons[i].addEventListener('click', function(){
+        if (this === buttons[answerButton]) {
+            answerMessage.innerHTML = "Correct!";
+        }
+        else {
+            answerMessage.innerHTML = "Wrong answer. Guess again!";
+        }
+
+    });
 }
+
