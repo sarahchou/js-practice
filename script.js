f798245cde479822bd339c012125ecc974c4c7d5
sarahchou <chou.s@husky.neu.edu>
2020-04-07 17:45:06 -0700

    adding reset button

--- script.js
+++ script.js
@@ -12,27 +12,35 @@ function makeColorValue() {
 var buttons = document.getElementsByClassName('colorButton');
 var heading = document.getElementById('colorValue');
 var answerMessage = document.getElementById('answer');
-var answerButton = Math.round(Math.random()*(buttons.length - 1));
 
-//Setting up each color value button
-for (var i = 0; i < buttons.length; i ++) {
-    var red = makeColorValue();
-    var green = makeColorValue();
-    var blue = makeColorValue();
-    setButtonColor(buttons[i], red, green, blue);
+function startGame() {
+    answerMessage.innerHTML = "";
+    var answerButton = Math.round(Math.random()*(buttons.length - 1));
+    
+    //Setting up each color value button
+    for (var i = 0; i < buttons.length; i ++) {
+        var red = makeColorValue();
+        var green = makeColorValue();
+        var blue = makeColorValue();
+        setButtonColor(buttons[i], red, green, blue);
 
-    if (i == answerButton) {
-        heading.innerHTML = `(${red},${green},${blue})`;
+        if (i == answerButton) {
+            heading.innerHTML = `(${red}, ${green}, ${blue})`;
+        }
+
+        buttons[i].addEventListener('click', function(){
+            if (this === buttons[answerButton]) {
+                answerMessage.innerHTML = "Correct!";
+            }
+            else {
+                answerMessage.innerHTML = "Wrong answer. Guess again!";
+            }
+
+        });
     }
 
-    buttons[i].addEventListener('click', function(){
-        if (this === buttons[answerButton]) {
-            answerMessage.innerHTML = "Correct!";
-        }
-        else {
-            answerMessage.innerHTML = "Wrong answer. Guess again!";
-        }
-
-    });
 }
+document.getElementById('resetButton').addEventListener('click', startGame);
+
+startGame();
 
